7ec206c4bd55c35d63620d8bceb0554b05bccccb
ProsperieEli <ElijahProsperie@gmail.com>
2021-10-11 17:45:55 -0400

    Endpoints finished

---
+++ data/todo.js
@@ -0,0 +1,25 @@
+const todoLists = [
+  {
+    job: 'Water plants',
+    status: false,
+    due: 'End of day' 
+    
+  },
+  {
+    job: 'Clean the kitchen',
+    status: false,
+    due: 'End of day'
+  },
+  {
+    job: 'Go to the doctor',
+    status: false,
+    due: 'End of day'
+  },
+  {
+    job: 'Set alarm for tomorrow',
+    status: false,
+    due: 'End of day'
+  }
+];
+
+module.exports = { todoLists };
